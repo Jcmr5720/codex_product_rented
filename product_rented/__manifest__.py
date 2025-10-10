c47b64ec6ae65063b1096e957a84b90f57082fe4
Jcmr5720 <camilomr1414@hotmail.com>
2025-10-10 15:27:05 -0500

    c

---
+++ product_rented/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'product_rented',
+    'summary': 'Modulo encargado de rentar productos.',
+    'version': '1.0',
+    'author': 'Juan Camilo Muñoz',
+    'category': 'Tools',
+    'depends': [
+        'loyalty',
+        'website',
+        'web',
+        'web_editor',
+        'portal',
+        'sale',
+        'website_sale',
+        'mail',
+        'product',
+    ],
+    'data': [
+
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            ('include', 'web._assets_bootstrap'),
+
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'GPL-3'
+}
