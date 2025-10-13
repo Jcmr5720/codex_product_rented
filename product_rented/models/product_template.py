d2995a31c77ca53e20ee37dcd0c0b6e2531947de
Juan Camilo Muñoz Rodríguez <132085703+Jcmr5720@users.noreply.github.com>
2025-10-13 00:37:45 -0500

    Add rent tab with pricing and description fields

--- product_rented/models/product_template.py
+++ product_rented/models/product_template.py
@@ -5,7 +5,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     rented_bolean = fields.Boolean(string="Puede ser rentado")
-    rented_prince = fields.Float(string="Precio de renta")
+    rented_price = fields.Float(string="Precio de renta", oldname="rented_prince")
     rented_time = fields.Char(string="Tiempo de renta")
     rented_description = fields.Text(string="Descripción de renta")
     rented_profit = fields.Float(string="Ganancia de renta")
