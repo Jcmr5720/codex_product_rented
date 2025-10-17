56e1432294a38bd388d793fe0bb6484b3a28dbf5
Jcmr5720 <camilomr1414@hotmail.com>
2025-10-17 10:12:15 -0500

    c website add

--- product_rented/controllers/main.py
+++ product_rented/controllers/main.py
@@ -3,11 +3,10 @@ from odoo.http import request
 
 
 class RentedProductController(http.Controller):
-    """Public controller that lists products that can be rented."""
 
     @http.route(['/rented_product'], type='http', auth='public', website=True, sitemap=False)
     def render_rented_products_page(self, **kwargs):
-        """Render the rented product listing for the website."""
+        
         rented_products = request.env['product.template'].sudo().search([
             ('rented_bolean', '=', True),
             ('website_published', '=', True),
