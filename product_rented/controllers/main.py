b37ebab7d6061e2f0619fb5364d7121b98dc169f
Juan Camilo Muñoz Rodríguez <132085703+Jcmr5720@users.noreply.github.com>
2025-10-17 13:37:06 -0500

    Fix rented price formatting and asset path

--- product_rented/controllers/main.py
+++ product_rented/controllers/main.py
@@ -1,4 +1,4 @@
-from odoo import http
+from odoo import http, tools
 from odoo.http import request
 
 
@@ -11,7 +11,16 @@ class RentedProductController(http.Controller):
             ('rented_bolean', '=', True),
             ('website_published', '=', True),
         ])
+        formatted_prices = {}
+        for product in rented_products:
+            if product.rented_price and product.currency_id:
+                formatted_prices[product.id] = tools.format_amount(
+                    request.env, product.rented_price, product.currency_id
+                )
+            else:
+                formatted_prices[product.id] = False
         values = {
             'products': rented_products,
+            'formatted_rented_prices': formatted_prices,
         }
         return request.render('product_rented.template_product_rented', values)
