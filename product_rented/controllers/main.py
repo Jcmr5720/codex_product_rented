81b3d5f4ed60ecde04c18e0da238c9adb3ab7bc3
Juan Camilo Muñoz Rodríguez <132085703+Jcmr5720@users.noreply.github.com>
2025-10-17 13:48:20 -0500

    Handle rented time price display safely

--- product_rented/controllers/main.py
+++ product_rented/controllers/main.py
@@ -1,4 +1,4 @@
-from odoo import http
+from odoo import http, tools
 from odoo.http import request
 
 
@@ -11,7 +11,44 @@ class RentedProductController(http.Controller):
             ('rented_bolean', '=', True),
             ('website_published', '=', True),
         ])
+        formatted_prices = {}
+        time_price_details = {}
+        for product in rented_products:
+            if product.rented_price and product.currency_id:
+                formatted_prices[product.id] = tools.format_amount(
+                    request.env, product.rented_price, product.currency_id
+                )
+            else:
+                formatted_prices[product.id] = False
+
+            details = []
+            for time_price in product.rented_time_price_ids:
+                option_name = (
+                    time_price.option_id.display_name
+                    or time_price.option_id.name
+                    or ""
+                )
+                final_price = time_price.final_price
+                has_price = final_price not in (None, False)
+                if has_price and product.currency_id:
+                    formatted_rate = tools.format_amount(
+                        request.env, final_price, product.currency_id
+                    )
+                else:
+                    formatted_rate = False
+                details.append(
+                    {
+                        "id": time_price.id,
+                        "option_name": option_name,
+                        "formatted_price": formatted_rate,
+                        "has_price": has_price,
+                        "raw_price": final_price,
+                    }
+                )
+            time_price_details[product.id] = details
         values = {
             'products': rented_products,
+            'formatted_rented_prices': formatted_prices,
+            'time_price_details': time_price_details,
         }
         return request.render('product_rented.template_product_rented', values)
